29de5c107100f78be675409f5a228600b572630e
h-okhs <h@okhs.jp>
2019-01-30 13:49:06 +0900

    sample

---
+++ ws/getBusPosition.py
@@ -0,0 +1,27 @@
+# coding: UTF-8
+import urllib2
+import re
+from bs4 import BeautifulSoup
+
+# アクセスするURL
+url = "http://transfer.navitime.biz/seibubus-dia/pc/location/BusLocationResult?startId=00110168&goalId=00110123"
+
+# URLにアクセスする htmlが帰ってくる → <html><head><title>経済、株価、ビジネス、政治のニュース:日経電子版</title></head><body....
+html = urllib2.urlopen(url)
+
+# htmlをBeautifulSoupで扱う
+soup = BeautifulSoup(html, "html.parser")
+
+plots = soup.find_all("li", id=re.compile("plot"))
+
+for plot in plots:
+  try:
+    plotsoup = BeautifulSoup(plot, "html.parser")
+    courseName = plotsoup.find_all("div", class_="courseName").pop(0).replace('＜', '').replace('＞', '')
+    departuretime = plotsoup.find_all("div", text=re.compile("発車まで")).pop(0).replace(' ', '')
+
+    print courseName
+    print departuretime
+  except:
+    pass
+
