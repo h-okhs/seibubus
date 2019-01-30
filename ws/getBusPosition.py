fba97e9ae1b515daa7aa8913dd46e154bfd150b5
h-okhs <h@okhs.jp>
2019-01-30 15:02:20 +0900

    1

--- ws/getBusPosition.py
+++ ws/getBusPosition.py
@@ -8,7 +8,8 @@ url = "http://transfer.navitime.biz/seibubus-dia/pc/location/BusLocationResult?s
 
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.80 Safari/537.36"}
 res = requests.get(url, headers=headers)
-s = etree.tostring(res.content, pretty_print=True).decode()
+hoge=etree.formstring(res.content)
+s = etree.tostring(hoge, pretty_print=True).decode()
 print(s)
 
 soup = BeautifulSoup(res.content, "html.parser")
