4e8f4dfa73d26aa8c94be0a58fa510a40e4279ca
h-okhs <h@okhs.jp>
2019-01-30 14:45:09 +0900

    1

--- ws/getBusPosition.py
+++ ws/getBusPosition.py
@@ -6,10 +6,12 @@ from bs4 import BeautifulSoup
 url = "http://transfer.navitime.biz/seibubus-dia/pc/location/BusLocationResult?startId=00110168&goalId=00110123"
 
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.80 Safari/537.36"}
-res = requests.get(url, timeout=1, headers=headers)
+res = requests.get(url, headers=headers)
 
-print(res.text)
-print(res.content)
+print(res.encoding)
+res.encoding='Shift-JIS'
+print(res.text.encode('utf-8'))
+
 
 soup = BeautifulSoup(res.content, "html.parser")
 
