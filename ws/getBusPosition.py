7d26e47925367fceae2c16ae9b201aa84be35638
h-okhs <h@okhs.jp>
2019-01-30 15:01:02 +0900

    1

--- ws/getBusPosition.py
+++ ws/getBusPosition.py
@@ -2,22 +2,18 @@
 import requests
 import re
 from bs4 import BeautifulSoup
+from lxml import etree
 
 url = "http://transfer.navitime.biz/seibubus-dia/pc/location/BusLocationResult?startId=00110168&goalId=00110123"
 
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.80 Safari/537.36"}
 res = requests.get(url, headers=headers)
-
-print(res.encoding)
-res.encoding='Shift-JIS'
-print(res.text.encode('utf-8'))
-
+s = etree.tostring(res.content, pretty_print=True).decode()
+print(s)
 
 soup = BeautifulSoup(res.content, "html.parser")
 
 plots = soup.select("ul#resultList li")
-
-print(plots.pop(0).find_all("div", class_="courseName"))
 
 for plot in plots:
   try:
